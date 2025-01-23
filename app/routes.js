cfcd6923cdab73157e9c7be9680b8219424668bd
mattnixon1 <159132354+mattnixon1@users.noreply.github.com>
2025-01-23 10:49:30 +0000

    add main pages

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,75 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/community/submit-new-component', (req, res) => {
+    res.redirect('/community/add-new-component/start');
+});
+
+router.post('/community/add-new-component/start', (req, res) => {
+    res.redirect('/community/add-new-component/user-details');
+});
+
+router.post('/community/add-new-component/user-details', (req, res) => {
+    res.redirect('/community/add-new-component/component-details');
+});
+
+router.post('/community/add-new-component/component-details', (req, res) => {
+    res.redirect('/community/add-new-component/component-screenshot');
+});
+
+router.post('/community/add-new-component/component-screenshot', (req, res) => {
+    res.redirect('/community/add-new-component/check-your-answers');
+});
+
+router.post('/community/add-new-component/check-your-answers', (req, res) => {
+    res.redirect('/community/add-new-component/confirmation');
+});
+
+router.post('/community/add-new-component/confirmation', (req, res) => {
+    res.redirect('/community/add-to-existing-component/share-code');
+});
+
+router.post('/community/add-to-existing-component/share-code', (req, res) => {
+    if (req.session.data['share-code'] === 'yes') {
+        res.redirect('/community/add-to-existing-component/add-code');
+    } else {
+        res.redirect('/community/add-to-existing-component/share-link');
+    }
+});
+
+router.post('/community/add-to-existing-component/add-code', (req, res) => {
+    res.redirect('/community/add-to-existing-component/share-link');
+});
+
+router.post('/community/add-to-existing-component/share-link', (req, res) => {
+    if (req.session.data['share-link'] === 'yes') {
+        res.redirect('/community/add-to-existing-component/add-link');
+    } else {
+        res.redirect('/community/add-to-existing-component/share-findings');
+    }
+});
+
+router.post('/community/add-to-existing-component/add-link', (req, res) => {
+    res.redirect('/community/add-to-existing-component/share-findings');
+});
+
+router.post('/community/add-to-existing-component/share-findings', (req, res) => {
+    if (req.session.data['share-link'] === 'yes') {
+        res.redirect('/community/add-to-existing-component/add-findings');
+    } else {
+        res.redirect('/community/add-to-existing-component/additional-info');
+    }
+});
+
+router.post('/community/add-to-existing-component/add-findings', (req, res) => {
+    res.redirect('/community/add-to-existing-component/additional-info');
+});
+
+router.post('/community/add-to-existing-component/additional-info', (req, res) => {
+    res.redirect('/community/add-to-existing-component/check-anwsers');
+});
+
+router.post('/community/add-to-existing-component/check-anwsers', (req, res) => {
+    res.redirect('/community/add-to-existing-component/confirmation');
+});
+
