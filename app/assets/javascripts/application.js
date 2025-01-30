300c4a244406a2bd94af60545f1f204a0ab656ef
mattnixon1 <159132354+mattnixon1@users.noreply.github.com>
2025-01-30 16:21:36 +0000

    fixed the character count issue
    Most of the gov components weren't working correctly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,9 @@
-//
 // For guidance on how to add JavaScript see:
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
-window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
-})
+// Ensure the DOM is fully loaded before running the script
+document.addEventListener('DOMContentLoaded', function() {
+  // Initialize all GOV.UK Frontend components
+  window.GOVUKFrontend.initAll();
+});
